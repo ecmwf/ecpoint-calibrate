26ff21bc1c46ca606a75b46a2dd4bc1a7f8291fb
Anirudha Bose <ani07nov@gmail.com>
2018-05-13 19:24:22 +0200

    Remove usage of React hot loader on top-level component

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 
 import Root from './config/Root';
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
+    <Component />,
     document.getElementById('root'),
   );
 };
 
 render(Root);
-
-if (module.hot) {
-  module.hot.accept('./config/Root', () => {
-    const newApp = require('./config/Root').default;
-    render(newApp);
-  });
-}
