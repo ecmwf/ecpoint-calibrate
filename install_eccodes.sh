6944469f961efb76553abaf52b52edfbde897967
Anirudha Bose <ani07nov@gmail.com>
2018-05-18 00:02:49 +0200

    Add Bash script to automatically install eccodes package

---
+++ install_eccodes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ECCODES_VERSION="2.7.3"
+ECCODES_SRC_DIR="eccodes-${ECCODES_VERSION}-Source"
+ECCODES_SRC_URL="https://software.ecmwf.int/wiki/download/attachments/45757960/${ECCODES_SRC_DIR}.tar.gz"
+
+wget $ECCODES_SRC_URL
+tar -xvzf  "$DIR/${ECCODES_SRC_DIR}.tar.gz"
+rm "$DIR/${ECCODES_SRC_DIR}.tar.gz"
+mkdir $DIR/build
+pushd $DIR/build
+cmake -DCMAKE_INSTALL_PREFIX=$DIR/vendor/eccodes -DCMAKE_BUILD_TYPE=Release ../$ECCODES_SRC_DIR
+make -j$(nproc)
+make install
+popd
+rm -Rf $DIR/build $DIR/${ECCODES_SRC_DIR}
