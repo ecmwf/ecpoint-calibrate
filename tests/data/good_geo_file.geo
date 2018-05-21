2dfc2d869fdf7bb39a72428d594d776f2fd9696c
Anirudha Bose <ani07nov@gmail.com>
2018-05-22 00:46:16 +0200

    Add basic unittest for GeopointsLoader class

---
+++ tests/data/good_geo_file.geo
@@ -0,0 +1,6 @@
+#GEO
+# lat   lon     height  date            time    value
+# Missing values represented by 3e+38 (not user-changeable)
+#DATA
+ -10.05 143.07  94182   20150610        4       0
+ -11.4  130.42  94119   20150610        19      0
---
+++ tests/test_loaders.py
@@ -0,0 +1,14 @@
+import os
+
+from core.loaders.GeopointsLoader import GeopointsLoader
+
+TEST_DATA_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'data'
+)
+
+
+def test_geopoints_loader_header_ok():
+    loader = GeopointsLoader(os.path.join(TEST_DATA_DIR, 'good_geo_file.geo'))
+    loader.validate()
+
+    assert len(list(loader.read())) == 2
