cbcc0e414e1ccdcb793771298f33663b73744305
Anirudha Bose <ani07nov@gmail.com>
2018-05-24 01:03:04 +0200

    Refactor GribLoader to use GribPoint and GribPoints classes + implement object protocols for math ops

--- core/loaders/GribLoader.py
+++ core/loaders/GribLoader.py
@@ -18,16 +18,92 @@ missingValue = 1e+20  # A value out of range
 
 
 @attr.s
-class GribData(object):
+class GribPoint(object):
     lat = attr.ib(converter=float)
     lon = attr.ib(converter=float)
     value = attr.ib(converter=float)
 
 
+class GribPoints(list):
+    def __sub__(self, other):
+        if not isinstance(other, GribPoints):
+            raise ValueError(
+                'Subtraction is allowed only between GribPoints objects.'
+            )
+
+        result = GribPoints()
+
+        for i, j in zip(self, other):
+            if i.lat != j.lat or i.lon != j.lon:
+                raise ValueError(
+                    'Index mismatch between GRIB points {} and {}'.format(i, j)
+                )
+
+            result.append(
+                GribPoint(lat=i.lat, lon=i.lon, value=i.value - j.value)
+            )
+
+        return result
+
+    def __add__(self, other):
+        if not isinstance(other, GribPoints):
+            raise ValueError(
+                'Addition is allowed only between GribPoints objects.'
+            )
+
+        result = GribPoints()
+
+        for i, j in zip(self, other):
+            if i.lat != j.lat or i.lon != j.lon:
+                raise ValueError(
+                    'Index mismatch between GribPoints {} and {}'.format(i, j)
+                )
+
+            result.append(
+                GribPoint(lat=i.lat, lon=i.lon, value=i.value + j.value)
+            )
+
+        return result
+
+    def __mul__(self, other):
+        if not isinstance(other, (int, float)):
+            raise ValueError(
+                'Multiplication is allowed only between GribPoints and int/float objects.'
+            )
+
+        result = GribPoints()
+
+        for grib_point in self:
+            result.append(
+                GribPoint(lat=grib_point.lat, lon=grib_point.lon, value=grib_point.value * other)
+            )
+
+        return result
+
+    def __div__(self, other):
+        if not isinstance(other, (int, float)):
+            raise ValueError(
+                'Division is allowed only between GribPoints and int/float objects.'
+            )
+
+        result = GribPoints()
+
+        for grib_point in self:
+            result.append(
+                GribPoint(lat=grib_point.lat, lon=grib_point.lon, value=grib_point.value / other)
+            )
+
+        return result
+
+    @property
+    def values(self):
+        return [item.value for item in self]
+
+
 class GribLoader(BaseLoader):
     def __init__(self, path):
         self.path = path
-        self.items = []
+        self.points = GribPoints()
         self.read()
 
     def read(self):
@@ -43,16 +119,12 @@ class GribLoader(BaseLoader):
 
                 [lat, lon, value] = result
 
-                self.items.append(
-                    GribData(lat=lat, lon=lon, value=value)
+                self.points.append(
+                    GribPoint(lat=lat, lon=lon, value=value)
                 )
 
             codes_grib_iterator_delete(iterid)
             codes_release(gid)
-
-    @property
-    def values(self):
-        return [item.value for item in self.items]
 
     def validate(self):
         pass
