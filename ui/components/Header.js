6122358c431ec3af7bb3cc52329529eec1fd03d1
Anirudha Bose <ani07nov@gmail.com>
2018-06-02 13:09:31 +0200

    Add dummy cards for selecting/previewing predictants using MDL

---
+++ ui/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="mdl-layout__header">
+      <div className="mdl-layout__header-row">
+        <span className="mdl-layout-title header-logo">
+            <img src="https://www.ecmwf.int/sites/all/themes/ecmwf_bootstrap_2017/dist/images/logo.production.png" />
+        </span>
+        <div className="mdl-layout-spacer" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
