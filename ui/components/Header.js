75c6f941ceb6f21c77e1a12e20c0c2fec5c1d052
Anirudha Bose <ani07nov@gmail.com>
2018-06-03 00:23:31 +0200

    Eslint fixes

--- ui/components/Header.js
+++ ui/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="mdl-layout__header">
       <div className="mdl-layout__header-row">
         <span className="mdl-layout-title header-logo">
-            <img src="https://www.ecmwf.int/sites/all/themes/ecmwf_bootstrap_2017/dist/images/logo.production.png" />
+          <img src="https://www.ecmwf.int/sites/all/themes/ecmwf_bootstrap_2017/dist/images/logo.production.png" />
         </span>
         <div className="mdl-layout-spacer" />
       </div>
