78ba0de52a4dae6c567308e254d15d5075da8439
Anirudha Bose <ani07nov@gmail.com>
2018-06-23 17:07:43 +0200

    Reformat test file

---
+++ tests/conf.py
@@ -0,0 +1,6 @@
+import os
+
+TEST_DATA_DIR = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'data'
+)
+
--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -1,20 +1,20 @@
 import os
 
-import numpy
-
 from core.loaders.GeopointsLoader import GeopointsLoader
-
-TEST_DATA_DIR = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), 'data'
-)
+from .conf import TEST_DATA_DIR
 
 
 def test_geopoints_loader_header_ok():
-    loader = GeopointsLoader(os.path.join(TEST_DATA_DIR, 'good_geo_file.geo'))
+    loader = GeopointsLoader(
+        os.path.join(TEST_DATA_DIR, 'good_geo_file.geo')
+    )
 
     assert len(loader.geopoints) == 2
 
-    loader = GeopointsLoader(os.path.join(TEST_DATA_DIR, 'good_geo_file_with_multiline_comments.geo'))
+    loader = GeopointsLoader(
+        os.path.join(TEST_DATA_DIR,
+                     'good_geo_file_with_multiline_comments.geo')
+    )
 
     assert len(loader.geopoints) == 2
 
