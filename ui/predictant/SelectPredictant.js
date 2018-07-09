7e86d55e94f0746e203a797a5320fb2eccbe5877
Anirudha Bose <ani07nov@gmail.com>
2018-07-09 23:35:51 +0200

    Replace MDL usage with pure Semantic UI React in SelectPredictant component

--- ui/predictant/SelectPredictant.js
+++ ui/predictant/SelectPredictant.js
@@ -1,118 +1,118 @@
 import React, { Component } from 'react'
 import { remote } from 'electron'
 
+import {
+  Grid,
+  Card,
+  Button,
+  Checkbox,
+  Icon,
+  Table,
+  Dropdown,
+  Input,
+  Label,
+  Radio,
+  Item,
+} from 'semantic-ui-react'
+
 const mainProcess = remote.require('./server')
 
 class SelectPredictant extends Component {
-  isPathFormatOK() {
-    return true
-  }
+  getPredictantField = () => (
+    <Item>
+      <Item.Content>
+        <Item.Header>
+          <h5>
+            Select directory for the predictants you want to use (rainfall,
+            temperature, etc.)
+          </h5>
+        </Item.Header>
+
+        <Item.Description>
+          <Button
+            onClick={() =>
+              this.props.onPredictantPathChange(mainProcess.selectDirectory())
+            }
+          >
+            Browse
+          </Button>
+        </Item.Description>
+        <Item.Extra>{this.props.predictant.predictantPath}</Item.Extra>
+      </Item.Content>
+    </Item>
+  )
+
+  getPredictorField = () => (
+    <Item>
+      <Item.Content>
+        <Item.Header>
+          <h5>
+            Select directory for the predictors you want to use (CAPE, SR, etc.)
+          </h5>
+        </Item.Header>
+
+        <Item.Description>
+          <Button
+            onClick={() =>
+              this.props.onPredictorsPathChange(mainProcess.selectDirectory())
+            }
+          >
+            Browse
+          </Button>
+        </Item.Description>
+        <Item.Extra>{this.props.predictant.predictorsPath}</Item.Extra>
+      </Item.Content>
+    </Item>
+  )
+
+  getPredictantTypeField = () => (
+    <Item>
+      <Item.Content>
+        <Item.Header>
+          <h5>Select the data type you want to load:</h5>
+        </Item.Header>
+
+        <Item.Description>
+          <Grid columns={2} padded>
+            <Grid.Column>
+              <Radio
+                label="GRIB"
+                value="grib"
+                checked={this.props.predictant.type === 'grib'}
+                onChange={() => this.props.onPredictantTypeChange('grib')}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Radio
+                label="NetCDF"
+                value="netcdf"
+                checked={this.props.predictant.type === 'netcdf'}
+                onChange={() => this.props.onPredictantTypeChange('netcdf')}
+              />
+            </Grid.Column>
+          </Grid>
+        </Item.Description>
+      </Item.Content>
+    </Item>
+  )
 
   render() {
     return (
-      <div className="mdl-grid">
-        <div className="mdl-layout-spacer" />
-        <div className="mdl-cell mdl-cell--4-col">
-          <div className="demo-card-square mdl-card mdl-shadow--2dp">
-            <div className="mdl-card__title mdl-card--expand">
-              <h2 className="mdl-card__title-text">Select Predictant</h2>
-            </div>
-            <div className="mdl-card__actions mdl-card--border">
-              <div className="mdl-card__supporting-text">
-                Select directory for the predictants you want to use (rainfall,
-                temperature, etc.)
-              </div>
-
-              <button
-                className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-                onClick={() =>
-                  this.props.onPredictantPathChange(
-                    mainProcess.selectDirectory()
-                  )
-                }
-              >
-                Browse
-              </button>
-              <span className="text-muted">
-                {this.props.predictant.predictantPath}
-              </span>
-
-              {!this.isPathFormatOK() && (
-                <p className="text-muted">
-                  The specified path does not conform with the required
-                  specification:
-                  <code>
-                    &lt;parent&gt;/vol/ecpoint/ecPoint_DB/FC/&lt;range
-                    identifier&gt;/tp
-                  </code>
-                </p>
-              )}
-            </div>
-
-            <div className="mdl-card__actions mdl-card--border">
-              <div className="mdl-card__supporting-text">
-                Select directory for the predictors you want to use (CAPE, SR,
-                etc.)
-              </div>
-
-              <button
-                className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-                onClick={() =>
-                  this.props.onPredictorsPathChange(
-                    mainProcess.selectDirectory()
-                  )
-                }
-              >
-                Browse
-              </button>
-              <span className="text-muted">
-                {this.props.predictant.predictorsPath}
-              </span>
-
-              {!this.isPathFormatOK() && (
-                <p className="text-muted">
-                  The specified path does not conform with the required
-                  specification:
-                  <code>
-                    &lt;parent&gt;/vol/ecpoint/ecPoint_DB/FC/&lt;range
-                    identifier&gt;/tp
-                  </code>
-                </p>
-              )}
-
-              <div className="mdl-card__supporting-text">
-                Select the data type you want to load:
-              </div>
-
-              <label className="mdl-radio mdl-js-radio mdl-js-ripple-effect type-selector">
-                <input
-                  type="radio"
-                  className="mdl-radio__button"
-                  value="grib"
-                  checked={this.props.predictant.type === 'grib'}
-                  onChange={e =>
-                    this.props.onPredictantTypeChange(e.target.value)
-                  }
-                />
-                <span className="mdl-radio__label">GRIB</span>
-              </label>
-              <label className="mdl-radio mdl-js-radio mdl-js-ripple-effect type-selector">
-                <input
-                  type="radio"
-                  className="mdl-radio__button"
-                  value="netcdf"
-                  checked={this.props.predictant.type === 'netcdf'}
-                  onChange={e =>
-                    this.props.onPredictantTypeChange(e.target.value)
-                  }
-                />
-                <span className="mdl-radio__label">NetCDF</span>
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="mdl-layout-spacer" />
-      </div>
+      <Grid container centered>
+        <Grid.Column>
+          <Card fluid color="teal">
+            <Card.Header>Select Predictant</Card.Header>
+            <Card.Content>
+              <Card.Description />
+              <Item.Group divided>
+                {this.getPredictantField()}
+                {this.getPredictorField()}
+                {this.getPredictantTypeField()}
+              </Item.Group>
+            </Card.Content>
+          </Card>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
