824429037fd37889dae9c9a0f6c7609c048bd052
Anirudha Bose <ani07nov@gmail.com>
2018-07-10 00:42:10 +0200

    Get rid of MDL stuff in App.js

--- ui/App.js
+++ ui/App.js
@@ -1,17 +1,13 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Predictant from './predictant'
 import Header from './header'
 
 const App = () => (
-  <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-tabs">
+  <Fragment>
     <Header />
-    <main className="mdl-layout__content">
-      <div className="page-content">
-        <Predictant />
-      </div>
-    </main>
-  </div>
+    <Predictant />
+  </Fragment>
 )
 
 export default App
