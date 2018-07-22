4aad9e79dd5f53603f3be12fce79a878b5a719b2
Anirudha Bose <ani07nov@gmail.com>
2018-07-22 16:14:08 +0200

    Convert Page component to a stateless functional one

--- ui/app/page/component.js
+++ ui/app/page/component.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import Step1 from '../../step1'
 import Step2 from '../../step2'
 import Step3 from '../../step3'
 
-class Page extends Component {
-  render () {
-    if (this.props.page === 0) {
-      return <Step1 />
-    }
-
-    if (this.props.page === 1) {
-      return <Step2 />
-    }
-
-    if (this.props.page === 2) {
-      return <Step3 />
-    }
+const Page = (props) => {
+  if (props.page === 0) {
+    return <Step1 />
+  }
+  if (props.page === 1) {
+    return <Step2 />
+  }
+  if (props.page === 2) {
+    return <Step3 />
   }
 }
 
