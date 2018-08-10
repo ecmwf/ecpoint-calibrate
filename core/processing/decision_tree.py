8f3ea6eb25010a3eb5b8e640acfd73dce2d85250
Anirudha Bose <ani07nov@gmail.com>
2018-08-10 15:04:41 +0530

    Remove decision tree assertions from the main module

--- core/processing/decision_tree.py
+++ core/processing/decision_tree.py
@@ -91,33 +91,3 @@ class DecisionTree(object):
                     )
                 )
             print()
-
-
-if __name__ == '__main__':
-    import pandas
-    df = pandas.read_csv(
-        os.path.join(os.path.dirname(__file__), 'WeatherTypes.csv'),
-        sep='\t'
-    )
-    np.set_printoptions(suppress=True)
-    thrL, thrH = df.iloc[:, ::2], df.iloc[:, 1::2]
-    dt = DecisionTree(thrL=thrL, thrH=thrH)
-    thrL_matrix, thrH_matrix = dt.create()
-
-    expected_thrL_matrix = [[-9999., -9999.,  5., -9999., -9999.],
-                            [-9999., -9999.,  5., -9999.,    70.],
-                            [-9999., -9999.,  5., -9999.,   275.],
-                            [-9999., -9999., 20., -9999., -9999.],
-                            [-9999., -9999., 20., -9999.,    70.],
-                            [-9999., -9999., 20., -9999.,   275.]]
-    assert np.array_equal(thrL_matrix, expected_thrL_matrix)
-
-    expected_thrH_matrix = [[0.25, 2., 20., 9999., 70.],
-                            [0.25, 2.,   20., 9999.,  275.],
-                            [0.25, 2.,   20., 9999., 9999.],
-                            [0.25, 2., 9999., 9999.,   70.],
-                            [0.25, 2., 9999., 9999.,  275.],
-                            [0.25, 2., 9999., 9999., 9999.]]
-    assert np.array_equal(thrH_matrix, expected_thrH_matrix)
-
-
