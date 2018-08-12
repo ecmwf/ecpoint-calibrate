47b21e6f9292cb6e7004857f12df9df719b88911
Anirudha Bose <ani07nov@gmail.com>
2018-08-13 00:37:41 +0530

    GH-22: Fix broken decision tree test

--- tests/test_decision_tree.py
+++ tests/test_decision_tree.py
@@ -9,28 +9,47 @@ from .conf import TEST_DATA_DIR
 
 
 def test_decision_tree_with_predefined_threshold_splits():
-    df = pandas.read_csv(os.path.join(TEST_DATA_DIR, "WeatherTypes.csv"), sep="\t")
+    records = [
+        ("-inf", "0.25", "-inf", "2", "5", "20", "-inf", "inf", "-inf", "70"),
+        ("", "", "", "", "20", "inf", "", "", "70", "275"),
+        ("", "", "", "", "", "", "", "", "275", "inf"),
+    ]
+
+    labels = [
+        "cpr_thrL",
+        "cpr_thrH",
+        "tp_thrL",
+        "tp_thrH",
+        "wspd_thrL",
+        "wspd_thrH",
+        "cape_thrL",
+        "cape_thrH",
+        "sr_thrL",
+        "sr_thrH",
+    ]
+
+    df = pandas.DataFrame.from_records(records, columns=labels)
 
     thrL, thrH = df.iloc[:, ::2], df.iloc[:, 1::2]
     dt = DecisionTree(thrL_in=thrL, thrH_in=thrH)
     dt.create()
 
     expected_thrL_matrix = [
-        [-9999., -9999., 5., -9999., -9999.],
-        [-9999., -9999., 5., -9999., 70.],
-        [-9999., -9999., 5., -9999., 275.],
-        [-9999., -9999., 20., -9999., -9999.],
-        [-9999., -9999., 20., -9999., 70.],
-        [-9999., -9999., 20., -9999., 275.],
+        [float("-inf"), float("-inf"), 5., float("-inf"), float("-inf")],
+        [float("-inf"), float("-inf"), 5., float("-inf"), 70.],
+        [float("-inf"), float("-inf"), 5., float("-inf"), 275.],
+        [float("-inf"), float("-inf"), 20., float("-inf"), float("-inf")],
+        [float("-inf"), float("-inf"), 20., float("-inf"), 70.],
+        [float("-inf"), float("-inf"), 20., float("-inf"), 275.],
     ]
     assert np.array_equal(dt.thrL_out, expected_thrL_matrix)
 
     expected_thrH_matrix = [
-        [0.25, 2., 20., 9999., 70.],
-        [0.25, 2., 20., 9999., 275.],
-        [0.25, 2., 20., 9999., 9999.],
-        [0.25, 2., 9999., 9999., 70.],
-        [0.25, 2., 9999., 9999., 275.],
-        [0.25, 2., 9999., 9999., 9999.],
+        [0.25, 2., 20., float("inf"), 70.],
+        [0.25, 2., 20., float("inf"), 275.],
+        [0.25, 2., 20., float("inf"), float("inf")],
+        [0.25, 2., float("inf"), float("inf"), 70.],
+        [0.25, 2., float("inf"), float("inf"), 275.],
+        [0.25, 2., float("inf"), float("inf"), float("inf")],
     ]
     assert np.array_equal(dt.thrH_out, expected_thrH_matrix)
