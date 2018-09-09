67772933b7716856960e3362aff51b85558e634c
Anirudha Bose <ani07nov@gmail.com>
2018-09-09 11:15:16 +0200

    Remove installation of eccodes as part of setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
 import os
-import shlex
-import subprocess
 import sys
 from codecs import open
 
-from setuptools import setup, find_packages
-from setuptools.command.develop import develop
-from setuptools.command.install import install
+from setuptools import find_packages, setup
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -17,76 +13,46 @@ class PreCompileSmokeTests(object):
 
     def __call__(self):
         if sys.version_info.major == 3:
-            raise self.PreCompilationError(
-                'Python 3 is not supported.'
-            )
-
-        if 'VIRTUAL_ENV' not in os.environ:
-            raise self.PreCompilationError(
-                'You need to activate a Python 2 virtualenv.'
-            )
-
-
-def extra_install_steps():
-    # Install eccodes
-    # [TODO] - Rewrite the install_eccodes.sh script in Python
-    cmd = shlex.split('bash install_eccodes.sh')
-    subprocess.Popen(args=cmd, cwd=here)
+            raise self.PreCompilationError("Python 3 is not supported.")
 
 
 PreCompileSmokeTests()
 
 
-def get_cmd_cls(base):
-    class Cmd(base):
-        def run(self):
-            extra_install_steps()
-            base.run(self)
-
-    return Cmd
-
-
 # Get the long description from the README file
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='ecpoint-cal',
-    version='0.0.1',
-    description='foo bar',
+    name="ecpoint-cal",
+    version="0.0.1",
+    description="foo bar",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/onyb/ecPoint-PyCal',
-    author='Anirudha Bose',
-    author_email='ani07nov@gmail.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/onyb/ecPoint-PyCal",
+    author="Anirudha Bose",
+    author_email="ani07nov@gmail.com",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Atmospheric Science',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='ecmwf ecpoint weather forecast',
+    keywords="ecmwf ecpoint weather forecast",
     packages=find_packages(),
-    cmdclass={
-        'develop': get_cmd_cls(develop),
-        'install': get_cmd_cls(install),
-    },
     install_requires=[
-        'attrs>=18.1.0',
-        'numpy>=1.14.3',
-        'flask>=1.0.2',
-        'pandas==0.17.1',
-        'matplotlib>=2.2.3'
+        "attrs>=18.1.0",
+        "numpy>=1.14.3",
+        "flask>=1.0.2",
+        "pandas==0.17.1",
+        "matplotlib>=2.2.3",
     ],
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage', 'pytest'],
-    },
+    extras_require={"dev": ["check-manifest"], "test": ["coverage", "pytest"]},
     project_urls={
-        'Bug Reports': 'https://github.com/onyb/ecPoint-PyCal/issues',
-        'Source': 'https://github.com/onyb/ecPoint-PyCal/',
-        'ECMWF': 'https://www.ecmwf.int'
+        "Bug Reports": "https://github.com/onyb/ecPoint-PyCal/issues",
+        "Source": "https://github.com/onyb/ecPoint-PyCal/",
+        "ECMWF": "https://www.ecmwf.int",
     },
 )
