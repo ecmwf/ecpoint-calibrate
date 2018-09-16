740c568c572660a9f05ff7850e135857cae2b1df
Anirudha Bose <ani07nov@gmail.com>
2018-09-16 22:32:39 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="ecpoint-cal",
-    version="0.1.1",
+    version="0.1.2",
     description="foo bar",
     long_description=long_description,
     long_description_content_type="text/markdown",
