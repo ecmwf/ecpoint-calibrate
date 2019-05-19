5e2806192b3ae46c24df2b35f8da3b681d1dccec
Anirudha Bose <anirudha.bose@alumni.cern>
2019-05-19 16:23:44 +0200

    Remove legacy loader modules

--- core/loaders/utils.py
+++
@@ -1,9 +0,0 @@
-from contextlib import contextmanager
-from multiprocessing import Pool, cpu_count
-
-
-@contextmanager
-def poolcontext():
-    pool = Pool(cpu_count() // 2)
-    yield pool
-    pool.terminate()
