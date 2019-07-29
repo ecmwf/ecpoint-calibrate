7341e8087da844d94a568cc2420ab8a03935f926
Anirudha Bose <anirudha.bose@alumni.cern>
2019-07-30 00:32:32 +0200

    Small wording change in computations page

--- ui/workflows/B/pages/2/computations/component.js
+++ ui/workflows/B/pages/2/computations/component.js
@@ -74,7 +74,7 @@ class Computation extends Component {
     return (
       <Table.Row positive={this.isPositive()}>
         <Table.Cell width={4}>
-          {this.props.index === 0 && <Label ribbon>Pre-computed variable</Label>}
+          {this.props.index === 0 && <Label ribbon>Predictand variable (pre-computed)</Label>}
           <p>Short name:</p>
           <Input
             fluid
