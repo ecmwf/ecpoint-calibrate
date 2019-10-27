072c931b7777622acef754520e5bb5e51fba0bdb
Anirudha Bose <anirudha.bose@alumni.cern>
2019-10-28 00:43:30 +0100

    Add http(s)_proxy and no_proxy env vars inside the electron service

--- go.sh
+++ go.sh
@@ -2,6 +2,12 @@
 
 xhost +SI:localuser:root
 
+if [ -z "$no_proxy" ] ; then
+  export no_proxy="core"
+else
+  export no_proxy="core,$no_proxy"
+fi
+
 rm -f ~/ecpoint.logs
 docker-compose down
 docker-compose pull
