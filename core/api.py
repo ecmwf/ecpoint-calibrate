c8c519cce62b73d836edc2211e6fb669e3736c98
Anirudha Bose <anirudha.bose@ledger.fr>
2020-10-25 02:25:19 +0100

    core: support reading from Parquet files in postprocessing

--- core/api.py
+++ core/api.py
@@ -12,6 +12,7 @@ from healthcheck import EnvironmentDump, HealthCheck
 from core.utils import sanitize_path
 from core.loaders.ascii import ASCIIDecoder
 from core.loaders.fieldset import Fieldset
+from core.loaders.parquet import ParquetPointDataTableReader
 from core.models import Config
 from core.postprocessors.decision_tree import DecisionTree, WeatherType
 from core.postprocessors.ks_engine import KolmogorovSmirnovEngine
@@ -55,7 +56,7 @@ def get_fields_from_ascii_table():
     payload = request.get_json()
     path = sanitize_path(payload["path"])
 
-    df = ASCIIDecoder(path=path).dataframe
+    df = load_dataframe(path)
     fields = set(df.columns) - {
         "BaseDate",
         "BaseTime",
@@ -150,7 +151,7 @@ def get_wt_histogram():
         payload["bins"],
     )
 
-    predictor_matrix = ASCIIDecoder(path=path).dataframe
+    predictor_matrix = load_dataframe(path)
 
     thrWT = [float(cell) for cell in thrWT]
     series = pandas.Series(dict(zip(labels, thrWT)))
@@ -179,7 +180,7 @@ def save_wt_histograms():
         payload["bins"],
     )
 
-    predictor_matrix = ASCIIDecoder(path=path).dataframe
+    predictor_matrix = load_dataframe(path)
 
     matrix = [[float(cell) for cell in row[1:]] for row in thrGridOut]
     df = pandas.DataFrame.from_records(matrix, columns=labels)
@@ -225,7 +226,7 @@ def get_error_rep():
 
     thrL, thrH = df.iloc[:, ::2], df.iloc[:, 1::2]
 
-    predictor_matrix = ASCIIDecoder(path=path).dataframe
+    predictor_matrix = load_dataframe(path)
     rep = DecisionTree.cal_rep_error(
         predictor_matrix, thrL_out=thrL, thrH_out=thrH, nBin=int(numCols)
     )
@@ -259,7 +260,7 @@ def get_breakpoints_suggestions():
         int(payload["numSubSamples"]),
     )
 
-    predictor_matrix = ASCIIDecoder(path=path).dataframe
+    predictor_matrix = load_dataframe(path)
 
     thrWT = [float(cell) for cell in thrWT]
     series = pandas.Series(dict(zip(labels, thrWT)))
@@ -301,7 +302,7 @@ def get_obs_frequency():
         payload["mode"],
     )
 
-    predictor_matrix = ASCIIDecoder(path=path).dataframe
+    predictor_matrix = load_dataframe(path)
 
     thrWT = [float(cell) for cell in thrWT]
     series = pandas.Series(dict(zip(labels, thrWT)))
@@ -326,6 +327,13 @@ def get_units(path):
 
     first_grib_file = next(base_predictor_path.glob("**/*.grib"))
     return Fieldset.from_path(first_grib_file).units
+
+
+def load_dataframe(path: str) -> pandas.DataFrame:
+    if path.endswith(".ascii"):
+        return ASCIIDecoder(path=path).dataframe
+    elif path.endswith(".parquet"):
+        return ParquetPointDataTableReader(path=path).dataframe
 
 
 def main():
