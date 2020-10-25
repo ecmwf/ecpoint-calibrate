3854a9b96918cf72da458e4bfd664d9f5b0e5dd3
Anirudha Bose <anirudha.bose@ledger.fr>
2020-10-25 19:24:13 +0100

    core: add an error_type property to point data loaders

--- core/loaders/__init__.py
+++ core/loaders/__init__.py
@@ -1,8 +1,14 @@
 import abc
 from dataclasses import dataclass
-from typing import List, Type
+from enum import Enum
+from typing import List
 
 import pandas
+
+
+class ErrorType(Enum):
+    FE = 1
+    FER = 2
 
 
 @dataclass
@@ -22,6 +28,17 @@ class BasePointDataReader(abc.ABC):
     @abc.abstractmethod
     def select(self, *args):
         raise NotImplementedError
+
+    @property
+    def error_type(self) -> ErrorType:
+        """
+        Returns an ErrorType enum indicating whether the point data table
+        contains a Forecast Error Ratio (FER) or Forecast Error (FE).
+
+        For optimal performance, self.columns should cache outputs in the
+        derived classes.
+        """
+        return ErrorType.FER if ErrorType.FER.name in self.columns else ErrorType.FE
 
 
 def load_point_data_by_path(path: str) -> BasePointDataReader:
