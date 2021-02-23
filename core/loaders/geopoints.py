c9a77947f3771fd3df62131b0d6fd299edaf9214
Anirudha Bose <anirudha.bose@ledger.fr>
2021-02-23 01:46:10 +0100

    core: add function to read units from geopoints file

--- core/loaders/geopoints.py
+++ core/loaders/geopoints.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import metview
@@ -16,3 +17,22 @@ def get_values(geopoints: metview.bindings.Geopoints) -> numpy.ndarray:
         return geopoints["value_0"]
 
     return geopoints.values()
+
+
+def read_units(path: Path) -> str:
+    with open(path) as f:
+        while line := f.readline():
+            if line.strip() == "#METADATA":
+                break
+
+            if line.strip() == "#DATA":
+                raise ValueError("units not found")
+
+        while line := f.readline():
+            if m := re.match(r"units=(.+)", line):
+                return m.group(1)
+
+            if line.strip() == "#DATA":
+                break
+
+    raise ValueError("units not found")
--- tests/unit/loaders/test_geopoints.py
+++ tests/unit/loaders/test_geopoints.py
@@ -1,3 +1,7 @@
+import textwrap
+
+import pytest
+
 from core.loaders import geopoints as geopoints_loader
 from tests.conf import TEST_DATA_DIR
 
@@ -23,3 +27,104 @@ def test_geopoints_loader_new_format_ok():
     loader = geopoints_loader.read(TEST_DATA_DIR / "new_geo_file_format.geo")
     assert len(loader) == 4
     assert geopoints_loader.get_values(loader).tolist() == [0.9, 0.3, 0.1, 0.9]
+
+
+def test_geopoints_units_ok(tmp_path):
+    path = tmp_path / "file.geo"
+    path.write_text(
+        textwrap.dedent(
+            """
+            #GEO
+            #FORMAT NCOLS
+            #COLUMNS
+            stnid	latitude	longitude	level	date	time	elevation	value_0
+            # Missing values represented by 3e+38 (not user-changeable)
+            #METADATA
+            date=20190101
+            k=43200
+            step=0
+            time=0000
+            units=mm
+            #DATA
+            21921	70.68	127.4	0	20190101	0	33	0.9
+            6370	51.45	5.38	0	20190101	0	22	0.3
+            """
+        ).strip()
+    )
+
+    units = geopoints_loader.read_units(path)
+    assert units == "mm"
+
+
+def test_geopoints_units_missing_metadata(tmp_path):
+    path = tmp_path / "file.geo"
+    path.write_text(
+        textwrap.dedent(
+            """
+            #GEO
+            #FORMAT NCOLS
+            #COLUMNS
+            stnid	latitude	longitude	level	date	time	elevation	value_0
+            # Missing values represented by 3e+38 (not user-changeable)
+            #DATA
+            21921	70.68	127.4	0	20190101	0	33	0.9
+            6370	51.45	5.38	0	20190101	0	22	0.3
+            """
+        ).strip()
+    )
+
+    with pytest.raises(ValueError):
+        geopoints_loader.read_units(path)
+
+
+def test_geopoints_units_missing_units(tmp_path):
+    path = tmp_path / "file.geo"
+    path.write_text(
+        textwrap.dedent(
+            """
+            #GEO
+            #FORMAT NCOLS
+            #COLUMNS
+            stnid	latitude	longitude	level	date	time	elevation	value_0
+            # Missing values represented by 3e+38 (not user-changeable)
+            #METADATA
+            date=20190101
+            k=43200
+            step=0
+            time=0000
+            #DATA
+            21921	70.68	127.4	0	20190101	0	33	0.9
+            6370	51.45	5.38	0	20190101	0	22	0.3
+            """
+        ).strip()
+    )
+
+    with pytest.raises(ValueError):
+        geopoints_loader.read_units(path)
+
+
+def test_geopoints_units_bad_comments_order(tmp_path):
+    path = tmp_path / "file.geo"
+    path.write_text(
+        textwrap.dedent(
+            """
+            #GEO
+            #FORMAT NCOLS
+            #COLUMNS
+            stnid	latitude	longitude	level	date	time	elevation	value_0
+            # Missing values represented by 3e+38 (not user-changeable)
+            #DATA
+            21921	70.68	127.4	0	20190101	0	33	0.9
+            6370	51.45	5.38	0	20190101	0	22	0.3
+            #METADATA
+            date=20190101
+            k=43200
+            step=0
+            time=0000
+            units=mm
+            """
+        ).strip()
+    )
+
+    with pytest.raises(ValueError):
+        geopoints_loader.read_units(path)
