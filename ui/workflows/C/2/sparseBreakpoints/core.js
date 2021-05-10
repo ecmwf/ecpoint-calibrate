1f03242fb796bc7d1f28ada17f6d89712b6e00f3
Anirudha Bose <anirudha.bose@ledger.fr>
2021-05-10 15:01:35 +0530

    Rewrite threshold breakpoints validator using a Stack

--- ui/workflows/C/2/sparseBreakpoints/core.js
+++ ui/workflows/C/2/sparseBreakpoints/core.js
@@ -2,6 +2,157 @@ import _ from 'lodash'
 
 import { realNumbers } from '~/utils/patterns'
 
+/**
+ * Helper to check if a given string is a valid element of the sequence.
+ *
+ * Example:
+ *   > isAllowedToken("inf")
+ *   true
+ *
+ *   > isAllowedToken("123.45")
+ *   true
+ *
+ *   > isAllowedToken("ani")
+ *   false
+ */
+const isAllowedToken = token =>
+  !['', null, undefined].includes(token) &&
+  (token === 'inf' || token === '-inf' || realNumbers.test(token))
+
+/**
+ * Define a helper to check if a > b.
+ *
+ * a = -inf and b = inf are also considered valid.
+ */
+const isGreaterThan = (a, b) => {
+  if (a === 'inf') {
+    return true
+  }
+
+  if (b === '-inf') {
+    return true
+  }
+
+  return parseFloat(a) > parseFloat(b)
+}
+
+const isGreaterThanOrEqual = (a, b) => a === b || isGreaterThan(a, b)
+
+/**
+ * Define a helper to check if a < b.
+ *
+ * a = -inf and b = inf are also considered valid.
+ */
+const isLessThan = (a, b) => {
+  if (a === '-inf') {
+    return true
+  }
+
+  if (b === 'inf') {
+    return true
+  }
+
+  return parseFloat(a) < parseFloat(b)
+}
+
+const isLessThanOrEqual = (a, b) => a === b || isLessThan(a, b)
+
+/**
+ * Stack based computer to evaluate the validity of the sequence.
+ */
+class Stack {
+  constructor(range) {
+    this.range = range
+    this.isLinear = _.isEqual(range, ['-inf', 'inf'])
+    this.stack = []
+    this.lastMaxPoppedElement = '-inf'
+  }
+
+  push(value) {
+    /**
+     * If value is an invalid token, the entire stack sequence is invalid.
+     */
+    if (!isAllowedToken(value)) {
+      throw `Invalid token: ${value}`
+    }
+
+    /**
+     * The value must be within the limits of the range.
+     */
+    const [low, high] = this.range
+    if (!(isGreaterThanOrEqual(value, low) && isLessThanOrEqual(value, high))) {
+      throw `Out of range: ${value}`
+    }
+
+    /**
+     * Empty stack - nothing to validate.
+     */
+
+    if (this.length() === 0) {
+      this.stack.push(value)
+      return
+    }
+
+    /**
+     * Stack has even number of elements - going to insert the low threshold.
+     */
+    if (this.length() % 2 === 0) {
+      if (this.top() !== value) {
+        throw `Non-contiguous insertion: ${value}`
+      }
+
+      /**
+       * Discard the top, and save the popped element if it is greater than max.
+       */
+      this.pop()
+      return
+    }
+
+    /**
+     * Stack has odd number of elements - going to insert the high threshold.
+     */
+    if (this.length() % 2 === 1) {
+      if (this.isLinear && !isGreaterThan(value, this.lastMaxPoppedElement)) {
+        throw `Non-increasing insertion: ${value}`
+      }
+
+      if (!this.isLinear && this.top() === value) {
+        this.pop()
+        return
+      }
+
+      this.stack.push(value)
+      return
+    }
+  }
+
+  pop() {
+    const el = this.stack.pop()
+
+    if (isGreaterThan(el, this.lastMaxPoppedElement)) {
+      this.lastMaxPoppedElement = el
+    }
+
+    return el
+  }
+
+  top() {
+    return this.stack.slice(-1)[0]
+  }
+
+  bottom() {
+    return this.stack.slice(0)[0]
+  }
+
+  equals(stack) {
+    return _.isEqual(stack, this.stack)
+  }
+
+  length() {
+    return this.stack.length
+  }
+}
+
 export const validateThresholdSequence = (sequence, range) => {
   /**
    * STEP 1: Define a helper to remove spaces from the end of the sequence.
@@ -33,146 +184,46 @@ export const validateThresholdSequence = (sequence, range) => {
   /**
    * STEP 3: Initialize the stack and populate it with the trimmed sequence.
    */
-  const stack = []
-  trimmedSequence.forEach(el => stack.push(el))
+  const stack = new Stack(range)
 
-  const isLinear = _.isEqual(range, ['-inf', 'inf'])
-
-  /**
-   * STEP 4: The first and last elements of the sequence should always be "-inf"
-   * and "inf" respectively; the sequence is otherwise invalid.
-   */
-  if (isLinear && (stack[0] !== '-inf' || stack.slice(-1)[0] !== 'inf')) {
+  try {
+    trimmedSequence.forEach(el => stack.push(el))
+  } catch {
     return false
   }
 
-  if (!isLinear && _.isEqual(range, trimmedSequence)) {
+  /**
+   * STEP 4: Linear sequences MUST evaluate to [-inf, inf]
+   */
+
+  if (stack.isLinear && !stack.equals(['-inf', 'inf'])) {
+    return false
+  }
+
+  /**
+   * STEP 5: Circular sequences MAY evaluate to the range.
+   */
+  if (!stack.isLinear && stack.equals(range)) {
     return true
   }
 
   /**
-   * STEP 5: Define a helper to check if a given string is a valid element of
-   * the sequence.
-   *
-   * Example:
-   *   > isAllowedToken("inf")
-   *   true
-   *
-   *   > isAllowedToken("123.45")
-   *   true
-   *
-   *   > isAllowedToken("ani")
-   *   false
+   * STEP 6: Circular sequences MAY evaluate to empty stack.
    */
-  const isAllowedToken = token =>
-    !['', null, undefined].includes(token) &&
-    (token === 'inf' || token === '-inf' || realNumbers.test(token))
-
-  /**
-   * STEP 6: Define a helper to check if a < b.
-   *
-   * a = -inf and b = inf are also considered valid.
-   */
-  const isLessThan = (a, b) => {
-    if (a === '-inf') {
-      return true
-    }
-
-    if (b === 'inf') {
-      return true
-    }
-
-    return parseFloat(a) < parseFloat(b)
+  if (!stack.isLinear && stack.length() === 0) {
+    return true
   }
 
   /**
-   * STEP 7: Circular  predictors  must have the same first and last elements
-   * in order to cover the entire range.
+   * STEP 7: all OK cases for circular sequences exhausted. The sequence
+   * is invalid.
    */
-  let previousLow = stack.slice(-1)[0]
-  const firstElement = stack[0]
-
-  if (
-    !isLinear &&
-    firstElement !== previousLow &&
-    firstElement !== range[0] &&
-    previousLow !== range[1]
-  ) {
+  if (!stack.isLinear) {
     return false
   }
 
   /**
-   * STEP 8: Stack based computer to evaluate the validity of the sequence.
-   *
-   * We loop over the stack until no elements are remaining.
-   */
-
-  while (stack.length !== 0) {
-    /**
-     * STEP 8.1: Get the (low, high) pair from the end of the sequence.
-     *
-     * NOTE: This mutates the stack.
-     */
-    const [high, low] = [stack.pop(), stack.pop()]
-
-    /**
-     * STEP 8.2: If low or high are unrecognized tokens, the entire sequence
-     * is invalid.
-     */
-    if (!isAllowedToken(high) || !isAllowedToken(low)) {
-      return false
-    }
-
-    /**
-     * STEP 8.3: low can never be "inf" and high can never be "-inf".
-     */
-    if (isLinear && (low === 'inf' || high === '-inf')) {
-      return false
-    }
-
-    /**
-     * STEP 8.4: low  should be  strictly less than high for linear
-     * predictors always, and only for the first row in case of circular
-     * predictors.
-     */
-    if (isLinear) {
-      if (!isLessThan(low, high)) {
-        return false
-      }
-    } else {
-      if (stack.length !== 0 && !isLessThan(low, high)) {
-        return false
-      }
-
-      if (isLessThan(low, range[0]) || isLessThan(high, range[0])) {
-        return false
-      }
-
-      if (firstElement === range[0]) {
-        if (!isLessThan(low, range[1]) && !isLessThan(high, range[1])) {
-          return false
-        }
-      } else if (!isLessThan(low, range[1]) || !isLessThan(high, range[1])) {
-        return false
-      }
-    }
-
-    /**
-     * STEP 8.5: the current high should be same as the previous high
-     * (in reverse stack order).
-     */
-    if (high !== previousLow) {
-      return false
-    }
-
-    /**
-     * STEP 8.6: save the low value for use in the next iteration.
-     */
-    previousLow = low
-  }
-
-  /**
-   * STEP 9: all KO cases exhausted. The sequence is valid.
+   * STEP 8: all KO cases exhausted. The sequence is valid.
    */
   return true
 }
